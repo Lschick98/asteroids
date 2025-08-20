53f1812a81e12e108faa573a65ae4ffa82217c2d
Lschick98 <lschick98@hotmail.com>
2025-08-19 20:34:00 -0500

    D: FPS Delta Time

--- main.py
+++ main.py
@@ -9,12 +9,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
     while 1 != 0:
         screen.fill((0,0,0))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = Clock.tick(60) / 1000.0
+
+      
 
 
 if __name__ == "__main__":
